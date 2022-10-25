74f17e79579e65481e0f4764fea31d9f39c4e0f3
Vasil <vrangelov85@abv.bg>
2022-10-25 15:44:53 +0300

    fixed album

--- music/my_album/forms.py
+++ music/my_album/forms.py
@@ -3,22 +3,30 @@ from django import forms
 from music.my_album.models import Album
 
 
-class AlbumForm(ModelForm):
+class AlbumBaseForm(ModelForm):
     class Meta:
         model = Album
         fields = "__all__"
         widgets = {
             "album_name": forms.TextInput(attrs={"placeholder": "Album Name"}),
             "artist": forms.TextInput(attrs={"placeholder": "Artist"}),
-            "description": forms.TextInput(attrs={"placeholder": "Description"}),
+            "description": forms.Textarea(attrs={"placeholder": "Description"}),
             "image_url": forms.URLInput(attrs={"placeholder": "Image URL"}),
             "price": forms.TextInput(attrs={"placeholder": "Price"}),
         }
 
 
-class DeleteAlbum(AlbumForm):
+class AlbumCreateForm(AlbumBaseForm):
+    pass
+
+
+class AlbumEditForm(AlbumBaseForm):
+    pass
+
+
+class AlbumDeleteAlbum(AlbumBaseForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for (_, field) in self.fields.items():
             field.widget.attrs["disabled"] = "disabled"
-            field.widget.attrs["readonly"] = "readonly"
+            # field.widget.attrs["readonly"] = "readonly"
--- music/my_album/views.py
+++ music/my_album/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 
-from music.my_album.forms import AlbumForm, DeleteAlbum
+from music.my_album.forms import AlbumCreateForm, AlbumEditForm, AlbumDeleteAlbum
 from music.my_album.models import Album
 
 
 def add_album(request):
-    form = AlbumForm()
+    form = AlbumCreateForm()
 
     if request.method == "POST":
-        form = AlbumForm(request.POST)
+        form = AlbumCreateForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home-page")
@@ -17,30 +17,30 @@ def add_album(request):
 
 
 def album_details(request, pk):
-    album = Album.objects.get(id=pk)
+    album = Album.objects.get(pk=pk)
     context = {"album": album}
     return render(request, "my_album/album-details.html", context)
 
 
 def edit_album(request, pk):
-    album = Album.objects.get(id=pk)
-    form = AlbumForm(instance=album)
+    album = Album.objects.get(pk=pk)
+    form = AlbumEditForm(instance=album)
 
     if request.method == "POST":
-        form = AlbumForm(request.POST, instance=album)
+        form = AlbumEditForm(request.POST, instance=album)
         if form.is_valid():
             form.save()
             return redirect("home-page")
-    context = {"form": form}
+    context = {"form": form, "album": album}
     return render(request, "my_album/edit-album.html", context)
 
 
 def delete_album(request, pk):
-    album = Album.objects.get(id=pk)
-    form = DeleteAlbum(instance=album)
+    album = Album.objects.get(pk=pk)
+    form = AlbumDeleteAlbum(instance=album)
 
     if request.method == "POST":
         album.delete()
         return redirect("home-page")
-    context = {"form": form}
+    context = {"form": form, "album": album}
     return render(request, "my_album/delete-album.html", context)
